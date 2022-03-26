fa43a0892f539ad44417217c5318ed15bfaf243f
Miramed <mr.tekin2112@gmail.com>
2022-03-26 15:10:44 -0500

    wrote the code for First Class

--- src/day0/First.java
+++ src/day0/First.java
@@ -3,7 +3,13 @@ package day0;
 public class First {
     public static void main(String[] args) {
 
-//         TODO: Works on this tomorrow
-//               add some coll code
+        System.out.println("Java: ");
+        System.out.println("Working... ");
+
+
+
+        //  extra line 1
+        // extra line 2
+        // extra line 3
     }
 }
